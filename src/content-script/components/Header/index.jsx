857002d4920af09ff0afda6f5d17c9ad649b5a19
Christian Sletten Eide <Christian.Sletten.Eide@nrk.no>
2019-04-01 23:37:04 +0200

    set height of header to follow githubs header

--- src/content-script/components/Header/index.jsx
+++ src/content-script/components/Header/index.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import './header.scss'
 
 export default class Header extends React.Component {
+  componentDidMount () {
+    this.setHeight()
+    window.addEventListener('resize', this.setHeight)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.setHeight)
+  }
+
+  setHeight =() => {
+    const height = document.getElementsByClassName('Header-old')[0].offsetHeight
+
+    this.setState({ height })
+  }
+
   render () {
     return (
-      <header className='text-bold'>
+      <header className='text-bold' style={{ height: this.state.height }}>
         Github Sidebar
         <a href='#' className='iconBtn' onClick={this.props.showSettings}>
           <i className='icon icon-gear' />
