8771df69438cc54acb2e0a828530acb10df5612b
Christian Sletten Eide <Christian.Sletten.Eide@nrk.no>
2019-10-23 21:54:49 -0400

    split repo and type into two components

--- src/content-script/components/Repositories/repository.jsx
+++ src/content-script/components/Repositories/repository.jsx
@@ -1,69 +1,10 @@
 /** @jsx h */
 import { h, createRef } from 'preact'
 import { PureComponent } from 'preact/compat'
-import Item from './item.jsx'
+import Type from './type.jsx'
 import Icons from '../../images/svgs/icons.js'
 import Read from '../Read/index.jsx'
 import { repoHasUnreadItems } from '../../js/setBadge.js'
-
-class Type extends PureComponent {
-  render () {
-    const { settings, port, repo, type } = this.props
-
-    const itemData = {
-      issues: {
-        text: 'Issues',
-        url: 'issues'
-      },
-      pullRequests: {
-        text: 'Pull requests',
-        url: 'pulls'
-      }
-    }
-
-    const itemsShown = settings.numberOfItems >= repo.totalItems[type]
-      ? ''
-      : `${settings.numberOfItems} of `
-
-    const totalNrItems = repo.totalItems[type]
-    const nrOfItems = totalNrItems > 0 && `(${itemsShown}${repo.totalItems[type]})`
-
-    const item = itemData[type]
-    const url = `${repo.url}/${item.url}`
-
-    return (
-      <div className={type}>
-        <div className='itemHeading'>
-          <div className='grid-1' />
-
-          <div className='grid-1'>
-            <Icons icon={type} />
-          </div>
-
-          <div className='grid'>
-            <h4>
-              <a href={url}>{item.text} {nrOfItems}</a>
-            </h4>
-          </div>
-        </div>
-
-        {repo[type] && repo[type].length > 0 &&
-          <ul>
-            {repo[type].map(item => {
-              return (
-                <Item
-                  item={item}
-                  type={type}
-                  port={port}
-                  settings={settings}
-                />
-              )
-            })}
-          </ul>}
-      </div>
-    )
-  }
-}
 
 export default class Repository extends PureComponent {
   constructor () {
---
+++ src/content-script/components/Repositories/type.jsx
@@ -0,0 +1,64 @@
+/** @jsx h */
+import { h } from 'preact'
+import { PureComponent } from 'preact/compat'
+import Item from './item.jsx'
+import Icons from '../../images/svgs/icons.js'
+
+export default class Type extends PureComponent {
+  render () {
+    const { settings, port, repo, type } = this.props
+
+    const itemData = {
+      issues: {
+        text: 'Issues',
+        url: 'issues'
+      },
+      pullRequests: {
+        text: 'Pull requests',
+        url: 'pulls'
+      }
+    }
+
+    const itemsShown = settings.numberOfItems >= repo.totalItems[type]
+      ? ''
+      : `${settings.numberOfItems} of `
+
+    const totalNrItems = repo.totalItems[type]
+    const nrOfItems = totalNrItems > 0 && `(${itemsShown}${repo.totalItems[type]})`
+
+    const item = itemData[type]
+    const url = `${repo.url}/${item.url}`
+
+    return (
+      <div className={type}>
+        <div className='itemHeading'>
+          <div className='grid-1' />
+
+          <div className='grid-1'>
+            <Icons icon={type} />
+          </div>
+
+          <div className='grid'>
+            <h4>
+              <a href={url}>{item.text} {nrOfItems}</a>
+            </h4>
+          </div>
+        </div>
+
+        {repo[type] && repo[type].length > 0 &&
+          <ul>
+            {repo[type].map(item => {
+              return (
+                <Item
+                  item={item}
+                  type={type}
+                  port={port}
+                  settings={settings}
+                />
+              )
+            })}
+          </ul>}
+      </div>
+    )
+  }
+}
