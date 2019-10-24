9ef0a997e9a84c1469a2fd83bf7a47b4b207f823
Christian Sletten Eide <Christian.Sletten.Eide@nrk.no>
2019-10-23 21:51:50 -0400

    split links to repo and owner

--- src/content-script/components/Repositories/repository.jsx
+++ src/content-script/components/Repositories/repository.jsx
@@ -1,14 +1,15 @@
-import {Component, h, createRef, Fragment} from 'preact'
-import {PureComponent} from 'preact/compat'
+/** @jsx h */
+import { h, createRef } from 'preact'
+import { PureComponent } from 'preact/compat'
 import Item from './item.jsx'
 import Icons from '../../images/svgs/icons.js'
 import Read from '../Read/index.jsx'
-import { repoHasUnreadItems } from "../../js/setBadge.js"
+import { repoHasUnreadItems } from '../../js/setBadge.js'
 
-class Type extends PureComponent  {  
-  render() {
+class Type extends PureComponent {
+  render () {
     const { settings, port, repo, type } = this.props
-    
+
     const itemData = {
       issues: {
         text: 'Issues',
@@ -19,47 +20,49 @@ class Type extends PureComponent  {
         url: 'pulls'
       }
     }
-    
+
     const itemsShown = settings.numberOfItems >= repo.totalItems[type]
-    ? ""
-    : `${settings.numberOfItems} of `
-    
+      ? ''
+      : `${settings.numberOfItems} of `
+
     const totalNrItems = repo.totalItems[type]
     const nrOfItems = totalNrItems > 0 && `(${itemsShown}${repo.totalItems[type]})`
-    
+
     const item = itemData[type]
     const url = `${repo.url}/${item.url}`
-    
+
     return (
       <div className={type}>
-      <div className='itemHeading'>
-        <div className="grid-1"></div>
-        
-        <div className="grid-1">
-          <Icons icon={type} />
+        <div className='itemHeading'>
+          <div className='grid-1' />
+
+          <div className='grid-1'>
+            <Icons icon={type} />
+          </div>
+
+          <div className='grid'>
+            <h4>
+              <a href={url}>{item.text} {nrOfItems}</a>
+            </h4>
+          </div>
         </div>
 
-        <div className="grid">
-          <h4>
-            <a href={url}>{item.text} {nrOfItems}</a>
-          </h4>
-        </div>
+        {repo[type] && repo[type].length > 0 &&
+          <ul>
+            {repo[type].map(item => {
+              return (
+                <Item
+                  item={item}
+                  type={type}
+                  port={port}
+                  settings={settings}
+                />
+              )
+            })}
+          </ul>}
       </div>
-
-      {repo[type] && repo[type].length > 0 &&
-        <ul>
-          {repo[type].map(item => {
-            return <Item
-            item={item}
-            type={type}
-            port={port}
-            settings={settings}
-            />
-          })}
-        </ul>}
-    </div>
-  )
-}
+    )
+  }
 }
 
 export default class Repository extends PureComponent {
@@ -73,7 +76,7 @@ export default class Repository extends PureComponent {
     this.repoHeight = createRef()
   }
 
-  componentDidUpdate() {
+  componentDidUpdate () {
     const repoHeight = this.getRepoHeight()
     if (repoHeight !== this.state.repoHeight) {
       this.setState({ repoHeight })
@@ -81,23 +84,26 @@ export default class Repository extends PureComponent {
   }
 
   toggleCollapsed = () => {
-      this.props.port.postMessage({ type: 'toggleCollapsed', url: this.props.repo.url })
+    this.props.port.postMessage({ type: 'toggleCollapsed', url: this.props.repo.url })
   }
 
   toggleHover =() => {
-    this.setState({hover: !this.state.hover})
+    this.setState({ hover: !this.state.hover })
   }
 
-  stopPropagation(e) {
+  stopPropagation (e) {
+    // Prevent collapse on click
     e.stopPropagation()
   }
 
   renderItems (availableItems) {
     return availableItems.map(type => {
-      return <Type
-        type={type}
-        {...this.props}
-      />
+      return (
+        <Type
+          type={type}
+          {...this.props}
+        />
+      )
     })
   }
 
@@ -118,20 +124,23 @@ export default class Repository extends PureComponent {
     })
   }
 
+  getOwnerURL (url) {
+    // Remove last part of url (reponame) and return the rest
+    return url.replace(new RegExp('(.*/)[^/]+$'), '$1')
+  }
+
   render () {
-    const { repo, settings, port } = this.props
+    const { repo, settings } = this.props
     const { repoHeight } = this.state
 
-    let availableItems = settings.listItemOfType === 'all'
+    const availableItems = settings.listItemOfType === 'all'
       ? ['issues', 'pullRequests']
       : [settings.listItemOfType]
-
-    const name = <Fragment><span>{repo.owner} / </span>{repo.name}</Fragment>
 
     let maxHeight = {}
     if (repo.collapsed) {
       maxHeight = { maxHeight: '0px' }
-    } else if(repoHeight) {
+    } else if (repoHeight) {
       maxHeight = { maxHeight: `${repoHeight}px` }
     }
 
@@ -139,36 +148,45 @@ export default class Repository extends PureComponent {
 
     const items = availableItems.map(item => {
       const totalItems = repo.totalItems[item]
-      const typeText = item === "pullRequests" ? "pull requests" : item
+      const typeText = item === 'pullRequests' ? 'pull requests' : item
       return (<span title={`${totalItems} ${typeText}`}>{totalItems}</span>)
     })
 
     const repoCount = hasActiveElements &&
-      (  
-        <div className="repoCount">
+      (
+        <div className='repoCount'>
           {items}
         </div>
       )
-    
+
     const repoHasUnreads = repoHasUnreadItems(repo)
-    const mouseoverText = repoHasUnreads ? "Mark repo as read" : "Mark repo as unread"
+    const mouseoverText = repoHasUnreads ? 'Mark repo as read' : 'Mark repo as unread'
 
     return (
       <li className={`repository ${repo.collapsed ? 'collapsed' : ''}`}>
-        <div className={`repoHeading ${this.state.hover && "hideHover"}`} onClick={this.toggleCollapsed}>
-          <div className="grid-1" onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>
-           {hasActiveElements && <Read read={!repoHasUnreads} status={"DEFAULT"} title={mouseoverText} toggleRead={this.toggleRead} />}
+        <div className={`repoHeading ${this.state.hover && 'hideHover'}`} onClick={this.toggleCollapsed}>
+          <div className='grid-1' onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>
+            {hasActiveElements && <Read read={!repoHasUnreads} status='DEFAULT' title={mouseoverText} toggleRead={this.toggleRead} />}
           </div>
-           
-          <div className="grid-1 expand">
-            <Icons icon={'arrow'}  />
+
+          <div className='grid-1 expand'>
+            <Icons icon='arrow' />
           </div>
-          
-          <div className="grid">
+
+          <div className='grid'>
             <h3 className='text-truncate' onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>
-              <a href={repo.url} onClick={this.stopPropagation} className='text-truncate'
-                title={`$(repo.owner}/${repo.name}`} >
-                {name}
+              <a
+                href={this.getOwnerURL(repo.url)} onClick={this.stopPropagation} className='org'
+                title={repo.owner}
+              >
+                {repo.owner}
+              </a>
+              <span>/</span>
+              <a
+                href={repo.url} onClick={this.stopPropagation}
+                title={repo.name}
+              >
+                {repo.name}
               </a>
             </h3>
 
