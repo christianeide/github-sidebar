430766c9b8a67c6ab2e12242d2b6ac2e486c0178
Christian Sletten Eide <Christian.Sletten.Eide@nrk.no>
2019-10-30 22:17:28 -0400

    set up bump version script

---
+++ bump_version.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const semver = require('semver')
+
+const args = getProcessArgs()
+
+function getProcessArgs () {
+  return process.argv.slice(2).reduce((args, arg) => {
+    const [key, value = true] = arg.split('=')
+    args[key.replace(/^-*/g, '')] = value
+    return args
+  }, {})
+}
+
+function readFileSync (path) {
+  const pckgRaw = fs.readFileSync(path)
+  return JSON.parse(pckgRaw)
+}
+
+function writeFileSync (path, data) {
+  fs.writeFileSync(path, JSON.stringify(data, null, 2))
+}
+
+function bumpVersion (path, newVersion) {
+  const file = readFileSync(path)
+  file.version = newVersion
+  writeFileSync(path, file)
+}
+
+const currentVersion = readFileSync('package.json').version
+const newVersion = semver.inc(currentVersion, args.version)
+
+bumpVersion('package.json', newVersion)
+bumpVersion('package-lock.json', newVersion)
+bumpVersion('manifest.json', newVersion)
+
+console.log('Version bumped to ' + newVersion)
