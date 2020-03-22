52eca349ae945314684329d1fc546ef7c36652f2
Christian Sletten Eide <Christian.Sletten.Eide@nrk.no>
2020-03-22 21:31:18 +0100

    set up jest as testlibrary

---
+++ __mocks__/jest.setup.js
@@ -0,0 +1,16 @@
+// Mock chrome
+delete global.window.chrome;
+global.window = Object.create(window);
+global.window.chrome = {
+	runtime: {
+		connect: jest.fn(() => {
+			return {
+				postMessage: jest.fn(),
+				onMessage: {
+					addListener: jest.fn()
+				}
+			}
+		})
+	}
+};
+
