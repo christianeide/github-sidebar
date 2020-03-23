eac32e6b3cc465f3d552bca39a96b56e00b8c4b8
Christian Sletten Eide <Christian.Sletten.Eide@nrk.no>
2020-03-23 20:58:45 +0100

    convert to pure functions

--- src/content-script/components/Read/index.jsx
+++ src/content-script/components/Read/index.jsx
@@ -1,19 +1,15 @@
 import { h } from 'preact';
-import { PureComponent } from 'preact/compat';
 import './read.scss';
 
-export default class Read extends PureComponent {
-	render() {
-		const { read, toggleRead, title, status } = this.props;
-		const titleText = title || (read ? 'Mark as read' : 'Mark as unread');
-		return (
-			<div
-				className={`readButton ${read ? 'read' : 'notRead'}`}
-				onClick={toggleRead}
-				title={titleText}
-			>
-				<div className={`color ${status}`} />
-			</div>
-		);
-	}
+export default function Read({ read, toggleRead, title, status }) {
+	const titleText = title || (read ? 'Mark as read' : 'Mark as unread');
+	return (
+		<div
+			className={`readButton ${read ? 'read' : 'notRead'}`}
+			onClick={toggleRead}
+			title={titleText}
+		>
+			<div className={`color ${status}`} />
+		</div>
+	);
 }
--- src/content-script/components/Repositories/index.jsx
+++ src/content-script/components/Repositories/index.jsx
@@ -1,33 +1,33 @@
 import { h } from 'preact';
-import { PureComponent } from 'preact/compat';
 
 import Repository from './repository.jsx';
 import NoRepos from './noRepos.jsx';
 import './repositories.scss';
 
-export default class Repositories extends PureComponent {
-	render() {
-		const { repositories, onToggleSettings, settings, port } = this.props;
-
-		if (repositories.length === 0) {
-			return <NoRepos onToggleSettings={onToggleSettings} />;
-		}
-
-		return (
-			<main>
-				<ul className="repositories">
-					{repositories.map((repo) => {
-						return (
-							<Repository
-								key={repo.url}
-								repo={repo}
-								settings={settings}
-								port={port}
-							/>
-						);
-					})}
-				</ul>
-			</main>
-		);
+export default function Repositories({
+	repositories,
+	onToggleSettings,
+	settings,
+	port
+}) {
+	if (repositories.length === 0) {
+		return <NoRepos onToggleSettings={onToggleSettings} />;
 	}
+
+	return (
+		<main>
+			<ul className="repositories">
+				{repositories.map((repo) => {
+					return (
+						<Repository
+							key={repo.url}
+							repo={repo}
+							settings={settings}
+							port={port}
+						/>
+					);
+				})}
+			</ul>
+		</main>
+	);
 }
