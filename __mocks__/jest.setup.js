f8889fba55e02089c0c4fa6ef8a13444c869bf0b
Christian Sletten Eide <Christian.Sletten.Eide@nrk.no>
2020-09-12 22:28:04 +0200

    setup tests for backend

--- __mocks__/jest.setup.js
+++ __mocks__/jest.setup.js
@@ -1,1 +1,4 @@
 import '@testing-library/jest-dom';
+
+// For mocking out chrome API
+Object.assign(global, require('jest-chrome'));
--- src/background/__tests__/background.test.js
+++ src/background/__tests__/background.test.js
@@ -1,3 +1,55 @@
-describe('background', () => {
-	it('should expect exiting things!', () => {});
+import {} from '../background.js';
+import { chrome } from 'jest-chrome';
+
+describe('chrome.runtime.onConnect', () => {
+	it('should setup listeners to incoming ports!', () => {
+		const port = {
+			name: 'port_name',
+			disconnect: jest.fn(),
+			onMessage: {
+				addListener: jest.fn(),
+			},
+			postMessage: jest.fn(),
+		};
+
+		expect(chrome.runtime.onConnect.hasListeners()).toBe(true);
+		chrome.runtime.onConnect.callListeners(port);
+
+		expect(port.onMessage.addListener).toBeCalledTimes(1);
+	});
+
+	it('should setup listeners to incoming ports!', () => {
+		const port = {
+			name: 'port_name',
+			disconnect: jest.fn(),
+			onMessage: {
+				addListener: jest.fn(),
+			},
+			postMessage: jest.fn(),
+		};
+
+		expect(chrome.runtime.onConnect.hasListeners()).toBe(true);
+		chrome.runtime.onConnect.callListeners(port);
+
+		port.onMessage.callListeners({ type: 'init' });
+
+		expect(port.onMessage.addListener).toBeCalledTimes(1);
+	});
+});
+
+describe('chrome.tabs.onUpdated', () => {
+	it('should expect exiting things!', () => {
+		// const message = { greeting: 'hello?' };
+		// const response = { greeting: 'here I am' };
+		// const callbackSpy = jest.fn();
+		// // chrome.runtime.sendMessage.mockImplementation((message, callback) => {
+		// // 	callback(response);
+		// // });
+		// // chrome.runtime.sendMessage(message, callbackSpy);
+		// const tabID = 1;
+		// const changeInfo = { url: 'https://github.com/christianeide' };
+		// chrome.tabs.onUpdated.callListeners(1, changeInfo);
+		// expect(chrome.tabs.onUpdated.addListener).toBeCalledTimes(1);
+		// expect(callbackSpy).toBeCalledWith(response);
+	});
 });
