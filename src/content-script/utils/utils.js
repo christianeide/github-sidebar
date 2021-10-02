e3d9d1b147ca8da92df651d80abcbc28f6e4464e
Christian Sletten Eide <Christian.Sletten.Eide@nrk.no>
2021-10-02 03:02:06 +0200

    add debounce as external ref

--- src/content-script/utils/utils.js
+++ src/content-script/utils/utils.js
@@ -9,3 +9,12 @@ export function repoHasUnreadItems(repo) {
 		false
 	);
 }
+
+export function debounce(func, delay) {
+	let inDebounce;
+	return function (...args) {
+		const context = this;
+		clearTimeout(inDebounce);
+		inDebounce = setTimeout(() => func.apply(context, args), delay);
+	};
+}
